7e0c24b0ebcd2097517d379db801f299244203c3
lupita alvarez <alv.lu1305@gmail.com>
2021-09-03 12:41:53 -0700

    TASK 1&2 PASS

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,24 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const headerDiv = document.createElement('div');
+  const spanDate = document.createElement('span');
+  const headerTitle = document.createElement('h1');
+  const spanTemp = document.createElement('span');
+
+  headerDiv.classList.add('header');
+  spanDate.classList.add('date');
+  spanTemp.classList.add('temp');
+
+  headerTitle.textContent = title;
+  spanDate.textContent = date;
+  spanTemp.textContent = temp;
+
+  headerDiv.appendChild(spanDate);
+  headerDiv.appendChild(headerTitle);
+  headerDiv.appendChild(spanTemp);
+
+  return headerDiv
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +38,9 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const newHeader = Header();
+  document.querySelector(selector).appendChild(newHeader);
+  newHeader.appendChild(Header('Lambda Times'))
 }
 
 export { Header, headerAppender }
